0a7c5374eaa7dfa7fb4ca9ece8a3c5159de6d7b8
BaranyBenedek <benedekbarany@gmail.com>
2023-09-26 08:43:38 +0200

    Bejegyzes class létrehozása, inicializálás, példányosítás, getterek, setterek.

---
+++ Bejegyzes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_doga
+{
+    class Bejegyzes
+    {
+        private string szerzo;
+        private string tartalom;
+        private int likeok;
+        private DateTime letrejott;
+        private DateTime szerkesztve;
+
+        public Bejegyzes(string szerzo, string tartalom, int likeok, DateTime letrejott, DateTime szerkesztve)
+        {
+            this.szerzo = szerzo;
+            this.tartalom = tartalom;
+            this.likeok = 0;
+            this.letrejott = DateTime.Now;
+            this.szerkesztve = DateTime.Now;
+        }
+
+        public string Szerzo { get => szerzo;}
+        public string Tartalom { get => tartalom; set => tartalom = value; }
+        public int Likeok { get => likeok;}
+        public DateTime Letrejott { get => letrejott;}
+        public DateTime Szerkesztve { get => szerkesztve;}
+        public void Like()
+        {
+            this.likeok++;
+        }
+    }
+}
